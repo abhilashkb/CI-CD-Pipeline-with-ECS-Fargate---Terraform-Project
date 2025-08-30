f000b12685f1fb0f9ad6bbfd0bd1ae71643810c9
abhilashkb <abhilashkb@outlook.com>
2025-08-30 14:07:57 +0530

    added s3 bucket variables and output

---
+++ terraform-code/modules/s3/output.tf
@@ -0,0 +1,16 @@
+output "bucket_name" {
+  description = "The name of the S3 bucket."
+  value       = aws_s3_bucket.codecommit_bucket.bucket
+  
+}
+
+output "bucket_arn" {
+  description = "The ARN of the S3 bucket."
+  value       = aws_s3_bucket.codecommit_bucket.arn
+  
+}
+output "bucket_id" {
+  description = "The ID of the S3 bucket."
+  value       = aws_s3_bucket.codecommit_bucket.id
+  
+}
---
+++ terraform-code/modules/s3/variables.tf
@@ -0,0 +1,4 @@
+variable "project_name" {
+    description = "The name of the project to be used as a prefix for the S3 bucket."
+    type        = string
+}
